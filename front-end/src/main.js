5349fe1303d2f15c29c250b0a25a600365b7d206
Nikarous <maciej.lopal@gmail.com>
2021-08-18 21:55:59 +0200

    Working on front

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
+
+var store = {
+  debug: true,
+  state: {
+    message: 'Hello!'
+  },
+  setMessageAction (newValue) {
+    if (this.debug) console.log('setMessageAction triggered with', newValue)
+    this.state.message = newValue
+  },
+  clearMessageAction () {
+    if (this.debug) console.log('clearMessageAction triggered')
+    this.state.message = ''
+  }
+}
 
 new Vue({
   render: h => h(App),
